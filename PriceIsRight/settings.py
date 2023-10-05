1a4903e74cea9cd0ed299ee5c37c5fe27b916cf4
Drew Esse <andrewesse404@gmail.com>
2023-10-05 02:34:04 -0400

    static

--- PriceIsRight/settings.py
+++ PriceIsRight/settings.py
@@ -109,6 +109,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+if 'DYNO' in os.environ:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
